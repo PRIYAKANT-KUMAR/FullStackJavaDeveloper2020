78158b8eb966df73229f9b780c72c2b447a07009
Priyakant Kumar <javatechpk@gmail.com>
2020-10-29 19:37:13 +0530

    Added new line2

--- HelloWorld/src/com/git/example/HelloWorld.java
+++ HelloWorld/src/com/git/example/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello I am Java Project HelloWorld1");
+		System.out.println("Hello I am Java Project HelloWorld2"); //Added New Line2
 
 	}
 
